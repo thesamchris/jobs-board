ffb679b62a85f3436d07158ae7b1071e72cfd469
Sam Chris <thesamchris@gmail.com>
2024-08-27 22:40:25 +0800

    Solve N+1 problem for job->employer by eager loading

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,22 +1,13 @@
 <x-layout>
     <x-slot:title>Jobs</x-slot:title>
 
-    <ul role="list" class="divide-y divide-gray-100">
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li class="flex justify-between gap-x-6 py-5">
-                <a href="/jobs/{{ $job['id'] }}">
-                <div class="flex min-w-0 gap-x-4">
-                    <div class="min-w-0 flex-auto">
-                        <p class="text-sm font-semibold leading-6 text-gray-900">{{ $job['title'] }}</p>
-                        <p class="mt-1 truncate text-xs leading-5 text-gray-500">Pays {{ $job['salary'] }}</p>
-                    </div>
-                </div>
-                </a>
-                <!-- <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end"> -->
-                <!--     <p class="text-sm leading-6 text-gray-900">Co-Founder / CEO</p> -->
-                <!--     <p class="mt-1 text-xs leading-5 text-gray-500">Last seen <time datetime="2023-01-23T13:23Z">3h ago</time></p> -->
-                <!-- </div> -->
-            </li>
+            <a href="/jobs/{{ $job['id'] }}" class="px-4 py-2 border-gray-200 border block rounded-lg">
+                <p class="text-xs text-blue-600">{{ $job->employer->name }}</p>
+                <p class="text-sm font-semibold leading-6 text-gray-900">{{ $job['title'] }}</p>
+                <p class="truncate text-xs leading-5 text-gray-500">Pays {{ $job['salary'] }}</p>
+            </a>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
